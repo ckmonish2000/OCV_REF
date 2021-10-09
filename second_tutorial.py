9e7b2ee1c9cc3efa2b57cab57cabf167e1899b0d
monish karunakaran <ckmonish2000@gmail.com>
2021-10-09 20:45:15 +0530

    done with cv2 2ch

--- second_tutorial.py
+++ second_tutorial.py
@@ -9,6 +9,12 @@ print(type(image))
 # image shape
 print(image.shape)
 
+# copy part of the array and paste it somewhere
+
+tag = image[300:400,500:600]
+image[200:300,600:700] = tag
+
+# basically ur performing array manipulation by resetting valuess in that array position
 
 # display
 cv2.imshow('image',image)
