b735fd9f07a6c57d746b5104771276de1f98abb7
monish karunakaran <ckmonish2000@gmail.com>
2021-10-09 19:53:09 +0530

    load and display image

--- first_tutorial.py
+++ first_tutorial.py
@@ -1,3 +1,13 @@
 import cv2 
 
+# load image
+img = cv2.imread("./assets/anja.jpg", 1)
 
+
+
+
+
+# display image and wait until any btn is pressed to close
+cv2.imshow('img',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
